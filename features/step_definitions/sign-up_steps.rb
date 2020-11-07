3ad710f658f558568dc1e51feddcbfdbeff15dbf
eshaeffer <eshaeffer@uiowa.edu>
2020-11-07 12:47:13 -0600

    Created final version of feature definition for acceptance testing.

--- features/step_definitions/sign-up_steps.rb
+++ features/step_definitions/sign-up_steps.rb
@@ -1,9 +1,19 @@
+
+
 Given /the following users exist:/ do |user_table|
   user_table.each {|usr| User.create!(usr)}
 end
-Given /I'm on the signup page/ do
-  visit sign-up_path
+
+
+Given /I'm on the sign-up page/ do
+  visit users_path
 end
+
 When /^I try to create a new account with username, email, password: "(.*?)"$/ do |arg1|
   arg1 = arg1.split(",").map {|a| a.gsub(" ", "")}
+
+end
+
+Then /^I should see: "(.*?)"$/ do |arg1|
+
 end
