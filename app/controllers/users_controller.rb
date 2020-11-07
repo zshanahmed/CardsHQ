31870bbf09cc7d148184fed0a7d49b25641fb0b8
eshaeffer <eshaeffer@uiowa.edu>
2020-11-06 22:57:10 -0600

    Finally got my boi to not accept spaces only in the dang textboxes. This was accomplished via a static method in the Users class because the controller create method was gettin pretty hefty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,20 +6,13 @@ class UsersController < ApplicationController
   def new
   end
 
-
-
-
   def create
-    reg = /(^[a-zA-z0-9_]+$)/
-
-    blank = user_params[:username].eql?("") || user_params[:password].eql?("") || user_params[:email].eql?("")
-    good_format = user_params[:username] =~ reg || user_params[:email] =~ reg  || user_params[:password] =~ reg
-
-
-    if !good_format || blank ||
+    if User.valid_entry?(user_params)
       flash[:notice] = "Invalid entry in one of the text-boxes"
+
     elsif not User.where(:username => user_params[:username]).blank?
       flash[:notice] = "Username, \'#{user_params[:username]}\' has already been taken"
+
     else
       User.create!(user_params)
     end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  def self.valid_entry?(parameters)
+    reg = /(^[a-zA-z0-9_]+$)/
+
+    blank = parameters[:username].eql?("") || parameters[:password].eql?("") || parameters[:email].eql?("")
+    good_format = parameters[:username] =~ reg || parameters[:email] =~ reg  || parameters[:password] =~ reg
+    if ! blank
+      spaces = (parameters[:username].ord % 32) + (parameters[:email].ord % 32) + (parameters[:password].ord % 32)
+      spaces_only = spaces == 0
+    else
+      spaces_only = true
+    end
+    return !spaces_only || !good_format || blank
+
+  end
 end
