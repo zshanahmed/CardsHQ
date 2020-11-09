cb0af84ab0c8e9e6f52402173d0a4591d0f628a8
Aishwarya Sharma <aishwarya-sharma@uiowa.edu>
2020-11-09 15:31:26 -0600

    modified user.spec to not contain the monkey patch

--- spec/models/User_spec.rb
+++ spec/models/User_spec.rb
@@ -1,26 +1,7 @@
 require 'spec_helper'
 require 'rails_helper'
 
-#https://github.com/rails/rails/issues/34790
-#######MONKEY PATCH#########
-if RUBY_VERSION>='2.6.0'
-  if Rails.version < '5'
-    class ActionController::TestResponse < ActionDispatch::TestResponse
-      def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
-      end
-    end
-  else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
-  end
-end
-
 describe User do
-
-
 
   bad_entry = [{:username=>'', :password=>'', :email=>''},
               {:username=>'', :password=>'hello', :email=>'wello'},
