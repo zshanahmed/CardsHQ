4a029a9fe00a8a5dd5563ba091298bada02c32f8
Aishwarya Sharma <aishwarya-sharma@uiowa.edu>
2020-11-09 16:20:11 -0600

    changed yml file to contain a different command for test

--- spec/models/User_spec.rb
+++ spec/models/User_spec.rb
@@ -3,6 +3,23 @@ require 'rails_helper'
 
 describe User do
 
+  #https://github.com/rails/rails/issues/34790
+  #######MONKEY PATCH#########
+  if RUBY_VERSION>='2.6.0'
+    if Rails.version < '5'
+      class ActionController::TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          # hack to avoid MonitorMixin double-initialize error:
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
+      end
+    else
+      puts "Monkeypatch for ActionController::TestResponse no longer needed"
+    end
+  end
+
   bad_entry = [{:username=>'', :password=>'', :email=>''},
               {:username=>'', :password=>'hello', :email=>'wello'},
               {:username=>'sss', :password=>'', :email=>'bbbb'},
