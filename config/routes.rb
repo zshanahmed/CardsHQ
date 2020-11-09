8ad598624a1e7555938095b518f4676e89d91939
eshaeffer <eshaeffer@uiowa.edu>
2020-11-08 22:59:13 -0600

    Removed :sessions from routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :users , :sessions
+  resources :users
   match '/login', to: 'sessions#new', via: :get
   match '/login_create', to: 'sessions#create', via: :post
   match '/logout', to: 'sessions#destroy', via: :delete
