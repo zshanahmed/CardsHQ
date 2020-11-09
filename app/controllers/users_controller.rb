da43c2389cca29718e80379fd5ca3d4a48ec4b3b
eshaeffer <eshaeffer@uiowa.edu>
2020-11-08 22:50:09 -0600

    fixed error in redirect.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,16 +9,15 @@ class UsersController < ApplicationController
   def create
     if !User.valid_entry?(user_params)
       flash[:notice] = "Invalid entry in one of the text-boxes"
-
+      redirect_to request.referrer
     elsif not User.where(:username => user_params[:username]).blank?
       flash[:notice] = "Username, \'#{user_params[:username]}\' has already been taken"
-
+      redirect_to request.referrer
     else
       User.create!(user_params)
       flash[:notice] = "Account with Username \'#{user_params[:username]}\' has been created"
       redirect_to login_path
     end
-    redirect_to request.referrer
   end
 end
 
