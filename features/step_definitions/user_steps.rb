af01b232107819cdf2fa5064aa7e891a964b0220
eshaeffer <eshaeffer@uiowa.edu>
2020-11-10 20:56:52 -0600

    Changed the User controller to try and work with cucumber

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -53,7 +53,7 @@ And /^With password: "(.*?)"$/ do |arg1|
 end
 
 And /^I press Create my account$/ do
-  click_button "createUser"
+  click_button("UserCreate")
 end
 
 Then /^I should see: "(.*?)"$/ do |arg1|
