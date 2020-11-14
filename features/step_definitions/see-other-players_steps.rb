a8df1f5ae84b563a48ca00a6142b8f06340630dc
AFilerio <alfredofilerio@gmail.com>
2020-11-13 23:37:11 -0600

    started See-Other-Players steps for feature testing

---
+++ features/step_definitions/see-other-players_steps.rb
@@ -0,0 +1,11 @@
+
+
+
+Given /^I'm on the other-players page$/ do
+  visit other_player_path
+end
+
+Then /^I should see "(.*?)" with "(.*?)" cards $/ do |user_name, num_cards|
+  page.has_content?(user_name)
+  page.has_content?(num_cards)
+end
