405a5cc285379fa4bad4398b9a709be1fa4a6105
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2020-11-17 20:20:28 -0600

    Updated users_controller to update room_id for user who created the room

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,10 @@ class UsersController < ApplicationController
   def create
     if !User.valid_entry?(user_params)
       flash[:notice] = "Invalid entry in one of the text-boxes"
-      redirect_to request.referrer
+      redirect_to new_user_path
     elsif not User.find_by(username: user_params[:username]).blank?
       flash[:notice] = "Username, \'#{user_params[:username]}\' has already been taken"
-      redirect_to request.referrer
+      redirect_to new_user_url
     else
       @user = User.create_user!(user_params)
       flash[:notice] = "Account with Username \'#{user_params[:username]}\' has been created"
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ActiveRecord::Base
   def self.valid_entry?(parameters)
     reg = /(^[a-zA-z0-9_@.]+$)/
     valid = true
-
     parameters.each do |key,entry|
       if entry.blank? || !(entry =~ reg) || (entry.ord % 32) == 0
         valid = false
