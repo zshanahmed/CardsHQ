7b9686bbd8beab45fba34105d09ade7721d5bca8
afilerio <alfredofilerio@gmail.com>
2020-11-20 05:20:16 -0600

    added password to first Given statement

--- features/step_definitions/see-other-players_steps.rb
+++ features/step_definitions/see-other-players_steps.rb
@@ -1,6 +1,6 @@
 
 
-Given /^Given I'm logged in with "(.*?)" and in room "(.*?)"$/ do |username, roomname|
+Given /^I'm logged in with "(.*?)" with password "(.*?)" and in room "(.*?)"$/ do |username, roomname|
   visit login_path
   fill_in 'loginUser', with: username
   fill_in 'loginEmail', with: "123"
@@ -9,13 +9,6 @@ Given /^Given I'm logged in with "(.*?)" and in room "(.*?)"$/ do |username, roo
   click_button 'Create My Room'
 end
 
-# And /^I'm on the show page$/ do
-#   visit rooms_path
-# end
-#
-# When /^"(.*?)" draws "(.*?)" cards $/ do |user_name, num_cards|
-#
-# end
 
 Then /^I should see "(.*?)" with "(.*?)" cards $/ do |username, numcards|
   page.should have_xpath('//table#users/thead/tr/td#username', :text)
