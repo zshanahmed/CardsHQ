1fe040170d2d6c9df51dbff7412b4d375ebbe34f
afilerio <alfredofilerio@gmail.com>
2020-11-20 05:53:04 -0600

    passing steps

--- features/step_definitions/see-other-players_steps.rb
+++ features/step_definitions/see-other-players_steps.rb
@@ -1,17 +1,17 @@
 
 
-Given /^I'm logged in with "(.*?)" with password "(.*?)" and in room "(.*?)"$/ do |username, roomname|
+Given /^I'm logged in with "(.*?)" with password "(.*?)" and in room "(.*?)"$/ do |username, password,roomname|
   visit login_path
   fill_in 'loginUser', with: username
-  fill_in 'loginEmail', with: "123"
-  click_button 'Create My Room'
+  fill_in 'loginEmail', with: password
+  click_button 'login_submit'
+  click_on 'Create New Room'
   fill_in 'RoomName', with: roomname
   click_button 'Create My Room'
 end
 
 
-Then /^I should see "(.*?)" with "(.*?)" cards $/ do |username, numcards|
-  page.should have_xpath('//table#users/thead/tr/td#username', :text)
-  page.has_xpath?('//table#users/thead/tr/td#username', text: username)
-  page.has_xpath?('//table#users/thead/tr/td#username', text: numcards)
+Then /^I should see "(.*?)" with "(.*?)" cards$/ do |username, numcards|
+  page.should have_content(username)
+  page.should have_content(numcards)
 end
