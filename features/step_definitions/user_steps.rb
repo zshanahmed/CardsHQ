41b990c4f764e04feb572450556fdb14678a230f
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2020-11-20 00:47:20 -0600

    Resolved acceptance testing for Signup Feature

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -39,10 +39,10 @@ end
 
 When /^I enter username,email,password and press submit: "(.*?)"$/ do |arg1|
   args = arg1.split(',')
-  fill_in "username", :with => args[0]
-  fill_in "email", :with => args[1]
-  fill_in "password", :with => args[2]
-  click_button("UserCreate")
+  fill_in 'signupUser', with: args[0]
+  fill_in 'signupEmail', with: args[1]
+  fill_in 'signupPassword', with: args[2]
+  click_on 'Create my account'
 end
 
 Then /^I should see: "(.*?)"$/ do |arg1|
