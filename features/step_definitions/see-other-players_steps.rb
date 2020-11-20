74735f1a8da9c6f3d00fc84f247e1e119574efc4
afilerio <alfredofilerio@gmail.com>
2020-11-20 05:12:50 -0600

    added step to navigate to create room page with a logged in user

--- features/step_definitions/see-other-players_steps.rb
+++ features/step_definitions/see-other-players_steps.rb
@@ -1,19 +1,24 @@
 
-Given /^Given I'm logged in with GrumpyBunny$/ do
+
+Given /^Given I'm logged in with "(.*?)" and in room "(.*?)"$/ do |username, roomname|
   visit login_path
-  fill_in 'loginUser', with: "GrumpyBunny"
+  fill_in 'loginUser', with: username
   fill_in 'loginEmail', with: "123"
+  click_button 'Create My Room'
+  fill_in 'RoomName', with: roomname
+  click_button 'Create My Room'
 end
 
-And /^I'm on the show page$/ do
-  visit rooms_path
-end
+# And /^I'm on the show page$/ do
+#   visit rooms_path
+# end
+#
+# When /^"(.*?)" draws "(.*?)" cards $/ do |user_name, num_cards|
+#
+# end
 
-When /^"(.*?)" draws "(.*?)" cards $/ do |user_name, num_cards|
-
-end
-
-Then /^I should see "(.*?)" with "(.*?)" cards $/ do |user_name, num_cards|
-  page.has_content?(user_name)
-  page.has_content?(num_cards)
+Then /^I should see "(.*?)" with "(.*?)" cards $/ do |username, numcards|
+  page.should have_xpath('//table#users/thead/tr/td#username', :text)
+  page.has_xpath?('//table#users/thead/tr/td#username', text: username)
+  page.has_xpath?('//table#users/thead/tr/td#username', text: numcards)
 end
