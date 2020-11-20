03c3c51e0ba71ada4c68a204ab70a07d775f4234
eshaeffer <eshaeffer@uiowa.edu>
2020-11-20 03:22:11 -0600

    Retooled acceptance testing by changing buttons to submit tags. Also had to add a line to click an extra button and change what capybara is checking in the discard view.

--- features/step_definitions/play_steps.rb
+++ features/step_definitions/play_steps.rb
@@ -6,18 +6,19 @@ end
 
 
 When /^'(.*?)' discards '(.*?)' cards$/ do |username,number_cards|
+  click_link "discard"
   id = User.where(username: username).first.id
   hand = Hand.where(user_id: id)
-
   hand.each_with_index do |card,i|
     if i < (number_cards.to_i)
-      check("played_cards_#{card.card_id}")
+      check("discarded_#{card.card_id}")
     end
   end
-  click_button "discard"
+  click_button "discardMain"
 end
 
 Then /^'(.*?)' should only have '(.*?)' cards$/ do |username, number|
   id = User.where(username: username)
-  expect(Hand.where(user_id: id).length()).to be number
+  number = number.to_i
+  expect(Hand.where(user_id: id).length()).to eq(number)
 end
