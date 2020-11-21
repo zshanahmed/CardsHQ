0961b373a1b4c7647c79659e34a79bd08bb72de9
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2020-11-20 18:26:12 -0600

    Updated users controller to show flash when card added to hand

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -86,6 +86,16 @@ describe UsersController do
     post :join_room, user: {room_id: ''}
     expect(flash[:notice]).to match('Invitation token invalid!')
   end
+  it 'should allow user to draw a card' do
+    room_test = Room.create(name: 'testroom123')
+    Card.create_deck_for_room(room_test.id)
+    #create a user
+    test_user = User.create_user!(test_valid)
+    request.session[:session_token] = test_user.session_token
+    post :join_room, user: {room_id: room_test.invitation_token}
+    post :draw_card
+    expect(flash[:notice]).to match('Cards added to your hand')
+  end
 end
 
 
