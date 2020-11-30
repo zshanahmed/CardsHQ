6a31343b74581ecd43eea5d2e1055e82f031483c
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2020-11-30 14:24:56 -0600

    Changed nickname as username to user's profile name in create from auth hash method in user model to cater both facebook and twitter authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,10 +24,10 @@ class User < ActiveRecord::Base
   def self.find_or_create_from_auth_hash(auth_hash)
     user = where(provider: auth_hash.provider, uid: auth_hash.uid).first_or_create
     user.update(
-        username: auth_hash.info.nickname,
-        profile_image: auth_hash.info.image,
-        token: auth_hash.credentials.token,
-        secret: auth_hash.credentials.secret
+      username: auth_hash.info.name,
+      profile_image: auth_hash.info.image,
+      token: auth_hash.credentials.token,
+      secret: auth_hash.credentials.secret
     )
     user
   end
