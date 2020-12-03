a363bf14f0e644e19883f68e7abc1bbf438e9fa1
Aishwarya Sharma <aishwarya-sharma@uiowa.edu>
2020-12-03 17:51:43 -0600

    added new method for twitter to controller

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -16,7 +16,7 @@ class Users::OmniauthController < ApplicationController
 
   # twitter callback
   def twitter
-    @user = User.create_from_twitter_data(request.env['omniauth.auth'])
+    @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
       set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
