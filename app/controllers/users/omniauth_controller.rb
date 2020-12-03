b3855ee9086f2f79d12f51b39de5eba1023c18e8
Aishwarya Sharma <aishwarya-sharma@uiowa.edu>
2020-12-03 17:56:11 -0600

    changed the controller method to have proper flash message

--- app/controllers/users/omniauth_controller.rb
+++ app/controllers/users/omniauth_controller.rb
@@ -19,7 +19,10 @@ class Users::OmniauthController < ApplicationController
     @user = User.create_from_provider_data(request.env['omniauth.auth'])
     if @user.persisted?
       sign_in_and_redirect @user
-      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+      if is_navigational_format?
+        flash[:notice] = 'successful sign in'
+      end
+      #set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
     else
       flash[:error] = 'There was a problem signing you in through Twitter. Please register or try signing in later.'
       redirect_to new_user_registration_url
