4956864949baa46a9f7b4122e5c5ae0712a8a62a
Zeeshan Ahmed <zeeshan-ahmed@uiowa.edu>
2020-12-03 20:42:59 -0600

    Removed github and googleOauth2 as they are not used

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
   devise  :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :lockable, :timeoutable,
-          :omniauthable, omniauth_providers: [:facebook, :github, :google_oauth2, :twitter]
+          :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   def self.create_from_provider_data(provider_data)
   where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
