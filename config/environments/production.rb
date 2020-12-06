54344a53ea64c89872f56813a38666a1f4391a36
zshanahmed <zeeshan-ahmed@uiowa.edu>
2020-12-06 14:29:23 -0600

    Updated production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :user_name => ENV['SENDGRID_USERNAME'],
       :password => ENV['SENDGRID_API_KEY'],
-      :domain => 'selt-sp3.herokuapp.com',
+      :domain => 'heroku.com',
       :address => 'smtp.sendgrid.net',
       :port => 25,
       :authentication => :plain,
