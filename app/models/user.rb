77138f9f2fc88af74025df3eec82619209dd58f4
zshanahmed <zeeshan-ahmed@uiowa.edu>
2020-12-06 22:50:32 -0600

    Updated config to deploy to production

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,11 @@ class User < ActiveRecord::Base
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
     user.email = provider_data.info.email
-    user.username = user.email.split('@')[0]
+    user.username = if provider_data.provider == "twitter"
+                      provider_data.info.nickname
+                    else
+                      user.email.split('@')[0]
+                    end
     user.password = Devise.friendly_token[0, 20]
     user.skip_confirmation!
   end
