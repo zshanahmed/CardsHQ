3fc2607a37c43041c03cd7e28f4fce10e8d150c5
zshanahmed <zeeshan-ahmed@uiowa.edu>
2020-12-06 18:36:24 -0600

    Updated production.rb to point to correct host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,13 +53,13 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-      :user_name => ENV['SENDMAIL_USERNAME'],
-      :password => ENV['SENDMAIL_PASSWORD'],
-      :domain => 'heroku.com',
-      :address => 'smtp.gmail.com',
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
+      user_name:      ENV['SENDMAIL_USERNAME'],
+      password:       ENV['SENDMAIL_PASSWORD'],
+      domain:        'heroku.com',
+      address:       'smtp.gmail.com',
+      port:          '587',
+      authentication: :plain,
+      enable_starttls_auto: true
   }
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
