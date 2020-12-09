fe72c9fecb6174e210e24bcd6233fe0a8bf7aebe
zshanahmed <zeeshan-ahmed@uiowa.edu>
2020-12-09 14:46:13 -0600

    Updated port number for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
     password: ENV['SENDGRID_PASSWORD'],
     domain: 'selt-sp3-test.herokuapp.com',
     address: 'smtp.sendgrid.net',
-    port: '587',
+    port: '465',
     authentication: :plain,
     enable_starttls_auto: true
   }
