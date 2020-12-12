90e11785e8ef616c7ce4d65a3bb7417a19bbf0be
Aishwarya Sharma <aishwarya-sharma@uiowa.edu>
2020-12-12 15:16:19 -0600

    removed byebugs from these files

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,6 @@ class User < ActiveRecord::Base
           :omniauthable, omniauth_providers: [:facebook, :twitter]
 
   def self.create_from_provider_data(provider_data)
-    byebug
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | user |
     user.email = provider_data.info.email
     user.username = if provider_data.provider == "twitter"
--- spec/models/User_spec.rb
+++ spec/models/User_spec.rb
@@ -3,20 +3,6 @@ require 'rails_helper'
 
 describe User do
   it 'should create from provider data' do
-
-    # omniauth_hash_fb = { 'provider' => 'facebook',
-    #                      'uid' => '12345',
-    #                      'info' => {
-    #                          'name' => 'test',
-    #                          'email' => 'test@testsomething.com'
-    #                      },
-    #                      'extra' => {'raw_info' =>
-    #                                      { 'location' => 'Chicago'
-    #                                      }
-    #                      }
-    # }
-    # request.env['omniauth.auth'] = OmniAuth.config.mock_auth[:facebook]
-    byebug
     user = User.create_from_provider_data(OmniAuth.config.mock_auth[:facebook])
     expect(user.username).not_to be(nil)
     expect(user.email).not_to be(nil)
