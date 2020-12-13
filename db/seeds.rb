c9fff81d50695ef68752983d8f6b0c232ec284b1
zshanahmed <zeeshan-ahmed@uiowa.edu>
2020-12-12 20:53:47 -0600

    Removed seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-users = [
-    {:username => "GrumpyBunny", :email => "botiqueBooth@gmail.com", :password => '123123123', :confirmed_at=>"2020-12-09 01:31:06.594"},
-    {:username => "bablingCreek", :email => "creepyLawyer@creepyLawyer.gov", :password=>'123123123', :confirmed_at=>Date.today},
-    {:username => "WarmBlanket", :email => "Beethoven@vienna.edu", :password => '123123123', :confirmed_at=>Date.today},
-    {:username => "softPillow", :email => "saltedButterWasAMistake@walmart.com", :password => "123123123", :confirmed_at=>Date.today},
-    {:username => "rollingHills", :email => "300@thisIsSparta.com", :password => "123123123", :confirmed_at=>Date.today}
-]
-
-
-
-
-
-users.each do |users|
-  User.create!(users)
-end
+# users = [
+#     {:username => "GrumpyBunny", :email => "botiqueBooth@gmail.com", :password => '123123123', :confirmed_at=>"2020-12-09 01:31:06.594"},
+#     {:username => "bablingCreek", :email => "creepyLawyer@creepyLawyer.gov", :password=>'123123123', :confirmed_at=>Date.today},
+#     {:username => "WarmBlanket", :email => "Beethoven@vienna.edu", :password => '123123123', :confirmed_at=>Date.today},
+#     {:username => "softPillow", :email => "saltedButterWasAMistake@walmart.com", :password => "123123123", :confirmed_at=>Date.today},
+#     {:username => "rollingHills", :email => "300@thisIsSparta.com", :password => "123123123", :confirmed_at=>Date.today}
+# ]
+#
+#
+#
+#
+#
+# users.each do |users|
+#   User.create!(users)
+# end
