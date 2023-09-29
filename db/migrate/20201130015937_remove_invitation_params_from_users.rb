dc4264c4463c324e7011f70a8b10fd80ed995c54
Zeeshan Ahmed <zahmed@vidiumah.com>
2023-09-29 01:30:03 -0500

    Updated migration

--- db/migrate/20201130015937_remove_invitation_params_from_users.rb
+++ db/migrate/20201130015937_remove_invitation_params_from_users.rb
@@ -5,12 +5,12 @@ class RemoveInvitationParamsFromUsers < ActiveRecord::Migration
     add_column :users, :secret, :string
     add_column :users, :profile_image, :string
     # remove_column :users, :invited_by_id
-    remove_column :users, :invitations_count
-    remove_column :users, :invitation_limit
-    remove_column :users, :invited_by_type
-    remove_column :users, :invitation_sent_at
-    remove_column :users, :invitation_accepted_at
-    remove_column :users, :invitation_created_at
-    remove_column :users, :image
+    # remove_column :users, :invitations_count
+    # remove_column :users, :invitation_limit
+    # remove_column :users, :invited_by_type
+    # remove_column :users, :invitation_sent_at
+    # remove_column :users, :invitation_accepted_at
+    # remove_column :users, :invitation_created_at
+    # remove_column :users, :image
   end
 end
