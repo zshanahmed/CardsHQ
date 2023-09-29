88dd55b14978a67b95e11f0e400aaccfec9370f0
Zeeshan Ahmed <zahmed@vidiumah.com>
2023-09-29 13:16:14 -0500

    Fixed user id on score card

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -24,7 +24,8 @@ class RoomsController < ApplicationController
       gon.room_id = @current_user.room_id.to_s
       @num_cards = []
       @room.users.all.each do |user|
-        @num_cards.append([user.username, Hand.where(user_id: user.id, room_id: user.room_id).length, user.score])
+        user_id = user.email.split("@")[0]
+        @num_cards.append([user_id, Hand.where(user_id: user.id, room_id: user.room_id).length, user.score])
       end
       @hand = Hand.where(user_id: @current_user.id, room_id: @current_user.room_id)
       @score = @current_user.score
